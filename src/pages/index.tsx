493675e3af82ec8b606e82859910e70edfaeb40b
rellyso <rellysondouglas2015@gmail.com>
2021-04-08 21:54:50 -0300

    Creating index page using static site generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,68 @@
+import { GetStaticProps } from 'next';
 import Head from 'next/Head';
 
-export default function Home() {
+import { SubscribeButton } from '../components/SubscribeButton';
+import { stripe } from '../services/stripe';
+
+import styles from './home.module.scss';
+
+
+// Client Side Rendering CSR
+// Server Side Generation SSG
+// Static site Generation SSG
+
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: string;
+  }
+}
+
+
+export default function Home({ product }: HomeProps) {
   return (
     <>
       <Head>
-        <title>Início | ig.news</title>
+        <title>Home | ig.news</title>
       </Head>
 
-      <h1>
-        Hello <span>World</span>
-      </h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+          <span>👏 Hey, welcome</span>
+          <h1>News about the <span>React</span> world.</h1>
+
+          <p>
+            Get access to all the publications <br />
+            <span>for {product.amount} month</span>
+          </p>
+
+          <SubscribeButton priceId={product.priceId} />
+        </section>
+
+        <img src="/images/avatar.svg" alt="Girl coding" />
+      </main>
     </>
   )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const price = await stripe.prices.retrieve('price_1IdyAiAPI3HzQTFhr1hs4gl5', {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: new Intl.NumberFormat('en-US', {
+      style: 'currency',
+      currency: 'USD',
+    }).format(price.unit_amount / 100),
+  }
+
+  return {
+    props: {
+      product,
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
+
+  }
 }
