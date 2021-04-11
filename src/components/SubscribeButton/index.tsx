3ed5b7e9b404c79877743e03d34c75d6f4404406
rellyso <rellysondouglas2015@gmail.com>
2021-04-10 21:43:41 -0300

    added handle subscribe if user is connected

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,14 +1,31 @@
+import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss'
 
 interface SubscribeButtonProps {
   priceId: string;
 }
 
+// getServerSideProps (SSR)
+// getStaticProps (SSG)
+// API routes
+
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
+  const [session] = useSession();
+
+  function handleSubscribe() {
+    if (!session) {
+      signIn('github');
+      return;
+    }
+
+
+  }
+
   return (
     <button
       type="button"
       className={styles.subscribeButton}
+      onClick={handleSubscribe}
     >
       Subscribe now
     </button>
