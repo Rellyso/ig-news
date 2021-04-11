6ea0dee8a3bee7c191f8298c851c9d9180cb6fd6
rellyso <rellysondouglas2015@gmail.com>
2021-04-10 21:40:49 -0300

    getting queries from stripe

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import Providers from 'next-auth/providers'
+
+import { fauna } from '../../../services/fuana'
 
 export default NextAuth({
   providers: [
@@ -9,4 +13,43 @@ export default NextAuth({
       scope: 'read:user'
     }),
   ],
+  // jwt: {
+  //   signingKey: process.env.SIGNING_KEY,
+  // },
+  callbacks: {
+    async signIn(user, account, profile) {
+
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } }
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+              )
+            )
+          )
+        )
+
+        return true
+
+      } catch {
+
+      }
+    },
+  }
 })
