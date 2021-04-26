7a7dc12316891773a35b19c576289cbbd249ac5a
Rellyso <rellysondouglas2015@gmail.com>
2021-04-26 09:01:28 -0300

    Added ActiveLink to active SPA link without reload

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,6 @@
 import SignInButton from '../SignInButton';
+import { ActiveLink } from '../ActiveLink';
+
 import styles from './styles.module.scss';
 
 export default function Header() {
@@ -7,8 +9,12 @@ export default function Header() {
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ig.news" />
         <nav>
-          <a className={styles.active}>Home</a>
-          <a>Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
 
         <SignInButton />
