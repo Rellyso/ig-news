c7cc21a5596310b45c3a06bfa1627bbd17c8e8d4
Rellyso <rellysondouglas2015@gmail.com>
2021-04-26 12:14:00 -0300

    added verification to know if user is subscribed

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -5,6 +5,7 @@ import Providers from 'next-auth/providers'
 
 import { fauna } from '../../../services/fuana'
 
+
 export default NextAuth({
   providers: [
     Providers.GitHub({
@@ -17,6 +18,41 @@ export default NextAuth({
   //   signingKey: process.env.SIGNING_KEY,
   // },
   callbacks: {
+    async session(session) {
+      try {
+        const userActiveSubscription = await fauna.query(
+          q.Get(
+            q.Intersection([
+              q.Match(
+                q.Index('subscription_by_user_ref'),
+                q.Select(
+                  "ref",
+                  q.Get(
+                    q.Match(
+                      q.Index('user_by_email'),
+                      q.Casefold(session.user.email)
+                    )
+                  )
+                )
+              ),
+              q.Match(
+                q.Index('subscription_by_status'),
+                "active"
+              )
+            ])
+          )
+        )
+        return {
+          ...session,
+          activeSubscription: userActiveSubscription
+        };
+      } catch {
+        return {
+          ...session,
+          activeSubscription: null,
+        };
+      }
+    },
     async signIn(user, account, profile) {
 
       const { email } = user
