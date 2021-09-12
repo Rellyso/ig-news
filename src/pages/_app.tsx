be2a98999a5807795e9b16bb79aa11cae56de76c
Rellyson Douglas <rellysondouglas2015@gmail.com>
2021-09-12 07:41:50 -0300

    added loading bar progress on app ⚡

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
+import NProgress from 'nprogress';
 import { AppProps } from 'next/app';
-import Header from '../components/Header';
 import { Provider as NextAuthProvider } from 'next-auth/client'
+import { Router } from 'next/router';
+
+import Header from '../components/Header';
 
 import '../styles/global.scss';
+import '../styles/nprogress.scss';
+
+Router.events.on('routeChangeStart', (url) => {
+  console.log(`Loading: ${url}`)
+  NProgress.start();
+})
+
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
