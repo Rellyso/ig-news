4de751a1dedf67fcc4fda1aa5a5318f13829b6ef
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-01-07 07:20:58 -0300

    Added SignIn button component tests

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -5,7 +5,7 @@ import { signIn, signOut, useSession } from 'next-auth/client'
 
 import styles from './styles.module.scss'
 
-export default function SignInButton() {
+export function SignInButton() {
   const [session] = useSession()
 
   return session ? (
