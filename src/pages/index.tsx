de722d774839eb4c4697ea84eeb70d1ca5fcfbc5
Rellyson Douglas <rellysondouglas2015@gmail.com>
2022-06-28 16:44:21 -0300

    add custom production price

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -47,7 +47,7 @@ export default function Home({ product }: HomeProps) {
 
 export const getStaticProps: GetStaticProps = async () => {
 
-  const price = await stripe.prices.retrieve(String(process.env.NEXT_PUBLIC_STRIPE_PRODUCT_ID), {
+  const price = await stripe.prices.retrieve('price_1LFkAVAPI3HzQTFhICUKb2OD', {
     expand: ['product']
   })
 
